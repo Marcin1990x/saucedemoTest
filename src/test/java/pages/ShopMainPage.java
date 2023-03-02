10b3667f15da4cd7db2c0437f3a4f8e7c99f5dbe
Marcin1990x <marcin.konecki21@gmail.com>
2023-03-02 19:01:52 +0100

    Add FindBy and getter for list of products on shop main page

--- src/test/java/pages/ShopMainPage.java
+++ src/test/java/pages/ShopMainPage.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class ShopMainPage {
 
     @FindBy(xpath = "//span[text()='Products']")
     private WebElement shopMainPageHeading;
+
+    @FindBy(xpath = "//select")
+    private WebElement sortField;
+
+    @FindBy(xpath = "//div[@class='inventory_item_name']")
+    private List<WebElement> shopItemNames;
 
     private WebDriver driver;
 
@@ -20,4 +29,15 @@ public class ShopMainPage {
     public WebElement getShopMainPageHeading() {
         return shopMainPageHeading;
     }
+
+    public ShopMainPage filterProducts(String filterName) {
+        Select select = new Select(sortField);
+        select.selectByVisibleText(filterName);
+        return this;
+    }
+
+    public List<WebElement> getShopItemNames() {
+        return shopItemNames;
+    }
+
 }
