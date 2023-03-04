1407f84e125930d6becb225f1021c482fc1f6ce0
Marcin1990x <marcin.konecki21@gmail.com>
2023-03-04 21:15:48 +0100

    User class for using its data in classes

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,4 +1,40 @@
 package models;
 
 public class User {
+
+    private static String firstName = "Marian";
+    private static String lastName = "Best";
+    private static String postalCode = "14-100";
+    private static String username = "standard_user";
+    private static String password = "secret_sauce";
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+
+    public static String getFirstName() {
+        return firstName;
+    }
+
+    public static String getLastName() {
+        return lastName;
+    }
+
+    public static String getPostalCode() {
+        return postalCode;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+
 }
