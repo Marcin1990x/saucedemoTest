bdf4d037ce1c9070660168413d6591c6df60d1c1
Marcin1990x <marcingothico14>
2023-03-07 18:02:50 +0100

    added elements for removing from cart test - in progress

--- src/test/java/pages/YourCartPage.java
+++ src/test/java/pages/YourCartPage.java
@@ -5,10 +5,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YourCartPage {
 
     @FindBy(id = "checkout")
     private WebElement checkoutButton;
+
+    @FindBy(id = "remove-sauce-labs-backpack")
+    private WebElement removeBackpackButton;
+
+    @FindBy(xpath = "//div[@class='cart_item']")
+    private WebElement cartNotEmptyClass;
+
+/*    @FindBy(xpath = "//div[@class='cart_item']")
+    private List<WebElement> cartNotEmptyClassList;*/
 
     private WebDriver driver;
 
@@ -21,4 +33,18 @@ public class YourCartPage {
         checkoutButton.click();
         return new CheckoutPage(driver);
     }
+
+    public YourCartPage removeBackpackFromCart(){
+        removeBackpackButton.click();
+        return this;
+    }
+
+    private List<WebElement> test = new ArrayList<>();
+
+    public Integer checkEmptyCart() {
+
+        test.add(cartNotEmptyClass);
+        return test.size();
+    }
+
 }
