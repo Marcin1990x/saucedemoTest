d501ab1963e7a91b99213282233096ead5aba9e8
Marcin1990x <marcin.konecki21@gmail.com>
2023-03-08 18:59:03 +0100

    Added aventstack dependency Modify BaseTest for aventstack

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import utils.DriverFactory;
 import utils.PropertyLoader;
 
@@ -11,6 +15,22 @@ import java.io.IOException;
 public class BaseTest {
 
     protected WebDriver driver;
+
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentReports extentReports;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        htmlReporter.flush();
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup() throws IOException {
